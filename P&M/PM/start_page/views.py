9719da6e7ba8367438d8a60058f8df6bc0ad735f
Artsiom_PH <workartpharm@gmail.com>
2022-03-21 21:46:24 +0200

    исправления

--- P&M/PM/start_page/views.py
+++ P&M/PM/start_page/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
     return render(request, "start_page/start.html")
+def about(request):
+    return render(request, "start_page/about.html")
